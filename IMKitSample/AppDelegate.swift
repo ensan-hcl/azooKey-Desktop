0de6b6f16b7ea28f1b1c7ba948f378b594a78255
ensan <ensan.hcl.dev@gmail.com>
2021-09-25 23:29:44 +0900

    fix bug that the window automatically opens

--- IMKitSample/AppDelegate.swift
+++ IMKitSample/AppDelegate.swift
@@ -8,6 +8,22 @@
 import Cocoa
 import InputMethodKit
 
+// Necessary to launch this app
+class NSManualApplication: NSApplication {
+    private let appDelegate = AppDelegate()
+
+    override init() {
+        super.init()
+        self.delegate = appDelegate
+    }
+
+    required init?(coder: NSCoder) {
+        // No need for implementation
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var server = IMKServer()
     var candidatesWindow = IMKCandidates()
