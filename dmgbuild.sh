ab2cdc3d312978153d2fee3db7475bffb6853e40
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2024-03-24 19:39:33 +0900

    add simple installer

---
+++ dmgbuild.sh
@@ -0,0 +1,1 @@
+dmgbuild -s dmgbuildsettings.py -D app="/Users/miwa/Library/Developer/Xcode/DerivedData/azooKeyMac-fwhyofwizfzqocehzwmrgbwfzbce/Build/Products/Release/azooKeyMac.app" "azooKey" azooKey.dmg
