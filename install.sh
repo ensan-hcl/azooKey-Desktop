44a0e5447e6254c5238e3ad71a5c3134ae088a7c
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2024-04-22 00:44:15 +0900

    update install script

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ set -e
 xcodebuild -workspace azooKeyMac.xcodeproj/project.xcworkspace -scheme azooKeyMac clean archive -archivePath build/archive.xcarchive | xcpretty
 sudo rm -rf /Library/Input\ Methods/azooKeyMac.app
 sudo cp -r build/archive.xcarchive/Products/Applications/azooKeyMac.app /Library/Input\ Methods/
+pkill azooKeyMac
